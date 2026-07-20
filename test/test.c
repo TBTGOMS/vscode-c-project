cc5a2ba8a490eb1e494beedfcf5d357e34da0ebf
TBTGOMS <3580045981@qq.com>
2026-07-20 10:38:14 +0800

    init: C project setup

---
+++ test/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+#define LOVE 521
+
+int main() {
+    
+    while(1){
+    int a;
+    scanf("%d",&a);
+    if(a==1314){
+        printf("%d\n",LOVE);
+    }
+    else{
+        printf("THE LOVER NOT FOUND\n");
+    }
+    }
+
+}
+
