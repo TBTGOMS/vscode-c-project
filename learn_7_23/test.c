28fe512ad1bde169b98a7f07da7816a7606b7bef
TBTGOMS <3580045981@qq.com>
2026-07-23 21:39:16 +0800

    尝试新看到的代码

---
+++ learn_7_23/test.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+    long long a = 0;
+
+    long long b = 0;
+
+    while(scanf("%lld,%lld",&a,&b)!=EOF)
+
+    {
+        if(a==b)
+        {
+
+            printf("%lld=%lld\n",a,b);
+
+        }
+        else if(a>b)
+        {
+
+            printf("%lld>%lld\n",a,b);
+
+        }
+        else 
+        {
+
+             printf("%lld<%lld\n",a,b);
+
+        }
+    } 
+
+    return 0;
+
+}
